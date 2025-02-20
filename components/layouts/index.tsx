2701d9adf76ac03b23be4e1475ab929a74793f68
Endeyr <aaron.thompson42594@gmail.com>
2025-02-20 13:31:01 -0500

    feat: ui scaffolding

--- components/layouts/index.tsx
+++ components/layouts/index.tsx
@@ -2,12 +2,6 @@ interface Props {
   children: React.ReactNode;
 }
 const Layout = ({ children }: Props) => {
-  return (
-    <div className='min-h-[100dvh]'>
-      <main className='flex min-h-[90dvh] w-full flex-col items-center justify-center'>
-        {children}
-      </main>
-    </div>
-  );
+  return <>{children}</>;
 };
 export default Layout;
