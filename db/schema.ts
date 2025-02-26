69cc4cd24b9626a532627cc820f646431c304eaf
Endeyr <aaron.thompson42594@gmail.com>
2025-02-26 14:55:11 -0500

    fix: rm about page test

--- db/schema.ts
+++ db/schema.ts
@@ -1,6 +1,37 @@
-import { int, singlestoreTable, text } from 'drizzle-orm/singlestore-core';
+import {
+  index,
+  int,
+  singlestoreTable,
+  text,
+} from 'drizzle-orm/singlestore-core';
 export const users = singlestoreTable('users_table', {
   id: int('id').primaryKey().autoincrement(),
   name: text('name'),
   age: int('age'),
 });
+
+export const files = singlestoreTable(
+  'files_table',
+  {
+    id: int('id').primaryKey().autoincrement(),
+    name: text('name'),
+    url: text('url'),
+    parent: int('parent'),
+    size: int('size'),
+  },
+  (t) => {
+    return [index('parent_index').on(t.parent)];
+  }
+);
+
+export const folders = singlestoreTable(
+  'folders_table',
+  {
+    id: int('id').primaryKey().autoincrement(),
+    name: text('name'),
+    parent: int('parent'),
+  },
+  (t) => {
+    return [index('parent_index').on(t.parent)];
+  }
+);
