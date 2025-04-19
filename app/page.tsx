797e4bed2666e53d4e4bd033b49e961298d5695c
Endeyr <aaron.thompson42594@gmail.com>
2025-04-19 09:16:38 -0400

    fix: stying for home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
 export default function HomePage() {
-  return <div>Home Page</div>;
+  return (
+    <div className='flex min-h-screen items-center justify-center bg-gray-900 p-8 text-gray-100'>
+      <div>Home Page</div>
+    </div>
+  );
 }
